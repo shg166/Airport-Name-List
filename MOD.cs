e0c2d26b2fb9d3ab569d6b3467afa4d148f3a14a
adon <86583577+shg166@users.noreply.github.com>
2023-06-16 17:02:42 +0900

    Add Code & Name List

---
+++ MOD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework.UI;
+using ICities;
+
+namespace JP_AirportNameList
+{
+	public class JP_AirportNameList : IUserMod
+	{
+
+		public string Name
+		{
+			get { return "JP Airport Name List (CNL) v1.0"; }
+		}
+
+		public string Description
+		{
+			get { return "Custom Name Listsを使って、DLC Airportsの空港エリア名を変更します"; }
+		}
+
+	}
+}
